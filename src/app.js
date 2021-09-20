b9f9f8ad05e2c05a61b7a743083a3efbd3c06c8e
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-09-20 21:04:59 +0300

    done class 27

--- src/app.js
+++ src/app.js
@@ -1,57 +1,90 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-import './app.scss';
+import "./app.scss";
 
 // Let's talk about using index.js and some other name in the component folder
 // There's pros and cons for each way of doing this ...
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import Form from './components/form/form';
-import Results from './components/results/results';
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Form from "./components/form/form";
+import Results from "./components/results/results";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+            requestParams: {},
+            resultsIsLoading: "waiting for user input",
+        };
+    }
+
+    callApi = async (requestParams) => {
+        await this.setState({
+            resultsIsLoading: "loading",
+        });
+        try {
+            if ((await requestParams.method) === "GET") {
+                let resData = await axios.get(requestParams.url);
+                let data = resData.data;
+                console.log(`GET resData >>>>>> `, resData);
+                this.setState({ data, requestParams, resultsIsLoading: "done" });
+            } else if ((await requestParams.method) === "POST") {
+                let resData = await axios.post(requestParams.url);
+                let data = resData.data;
+                console.log(`POST resData >>>>>> `, resData);
+                this.setState({ data, requestParams, resultsIsLoading: "done" });
+            } else if ((await requestParams.method) === "PUT") {
+                let resData = await axios.put(requestParams.url);
+                let data = resData.data;
+                console.log(`PUT resData >>>>>> `, resData);
+                this.setState({ data, requestParams, resultsIsLoading: "done" });
+            } else if ((await requestParams.method) === "DELETE") {
+                let resData = await axios.delete(requestParams.url);
+                let data = resData.data;
+                console.log(`DELETE resData >>>>>> `, resData);
+                this.setState({ data, requestParams, resultsIsLoading: "done" });
+            } else {
+                console.log(`DEFAULT resData >>>>>> `, resData);
+                const data = {
+                    count: 2,
+                    results: [
+                        { name: "fake thing 1", url: "http://fakethings.com/1" },
+                        { name: "fake thing 2", url: "http://fakethings.com/2" },
+                    ],
+                };
+                const formData = {
+                    method: "Mock data GET",
+                    url: "https://pokeapi.co/api/v2/pokemon",
+                };
+                this.setState({ data, requestParams: formData });
+            }
+        } catch {
+          await this.setState({
+            resultsIsLoading: "error",
+          })
+        }
     };
-  }
 
-  callApi = async (requestParams) => {
-    // mock output
-    // if (requestParams.method === "GET") {
-    //   let resData = await axios.get(requestParams.url);
-    // }
-    
+    render() {
+        return (
+            <>
+                <Header />
+                <div id="mainSection">
+                    <div>Request Method: {this.state.requestParams.method}</div>
+                    <div>URL: {this.state.requestParams.url}</div>
+                    <Form handleApiCall={this.callApi} />
+                    <Results data={this.state.data} resultsIsLoading={this.state.resultsIsLoading} />
+                </div>
 
-    const data = {
-      count: 2,
-      results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-      ],
-    };
-    await this.setState({data, requestParams});
-  }
-
-  render() {
-    return (
-      <>
-        <Header />
-        <div>Request Method: {this.state.requestParams.method}</div>
-        <div>URL: {this.state.requestParams.url}</div>
-        <Form handleApiCall={this.callApi} />
-        <Results data={this.state.data} />
-        <Footer />
-      </>
-    );
-  }
+                <Footer />
+            </>
+        );
+    }
 }
 
 export default App;
-
 
 // import React, { useState } from "react";
 
--- src/components/form/form.js
+++ src/components/form/form.js
@@ -4,27 +4,35 @@ import "./form.scss";
 export default function Form(props) {
     const [callMethod, setCallMethod] = useState("GET");
 
-    function handleSubmit (e) {
+    function handleSubmit(e) {
         e.preventDefault();
         const formData = {
             method: callMethod,
             url: e.target.url.value,
         };
         props.handleApiCall(formData);
-    };
+    }
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <label>
                     <span>URL: </span>
-                    <input name="url" type="text" />
+                    <input name="url" type="text" onFocus={(event) => event.target.select()} defaultValue="https://pokeapi.co/api/v2/pokemon" />
                     <button type="submit">GO!</button>
                 </label>
                 <label className="methods">
-                    <span id="get" onClick={() => setCallMethod("GET")}>GET</span>
-                    <span id="post" onClick={() => setCallMethod("POST")}>POST</span>
-                    <span id="put" onClick={() => setCallMethod("PUT")}>PUT</span>
-                    <span id="delete" onClick={() => setCallMethod("DELETE")}>DELETE</span>
+                    <span id="get" onClick={() => setCallMethod("GET")}>
+                        GET
+                    </span>
+                    <span id="post" onClick={() => setCallMethod("POST")}>
+                        POST
+                    </span>
+                    <span id="put" onClick={() => setCallMethod("PUT")}>
+                        PUT
+                    </span>
+                    <span id="delete" onClick={() => setCallMethod("DELETE")}>
+                        DELETE
+                    </span>
                 </label>
             </form>
         </>
